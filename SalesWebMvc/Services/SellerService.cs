e59e11d02b40a58e5c8ba67ecc60d0a87595d142
Elmo Junior <elmosouzajunior@gmail.com>
2021-04-25 10:01:41 -0300

    Implementação do Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,22 +1,41 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
-    public class DepartmentService
+    public class SellerService
     {
         private readonly SalesWebMvcContext _context;
 
-        public DepartmentService(SalesWebMvcContext context)
+        public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public List<Seller> FindAll()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return _context.Seller.ToList();
+        }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
         }
     }
 }
